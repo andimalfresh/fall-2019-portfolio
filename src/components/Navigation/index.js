88069915e93b14c050ffa8bcc7da466abc1ff4b9
unknown <w.andrew.pedersen@gmail.com>
2019-08-29 12:48:51 -0600

    Basic Layout styling

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div className="navigationDiv">
+            <div className="navigationElement"><button>ABOUT</button></div>
+            <div className="navigationElement"><button>PORTFOLIO</button></div>
+            <div className="navigationElement"><button>CONTACT</button></div>
+      </div>
+    ) 
+}
+
+export default Navigation
