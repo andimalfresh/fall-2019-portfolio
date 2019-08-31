48aa7ac2078d6844917b4dc8a72a540937a731a4
unknown <w.andrew.pedersen@gmail.com>
2019-08-31 14:07:51 -0600

    Added job Pics and changed text, tried to implement funtions but reverted

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,19 @@
 import React from 'react'
 
-const About = () => {
+const About = (props) => {
     return (
         <div className='page-Setup'>
-          <h2>
-            About <span className='nameAndrew'>ANDREW</span>
-          </h2>
+        <span className='nameAndrew'> ABOUT ANDREW</span>
           <div className='pageCard'>
-            <h3>
-                Professional History
-            </h3>
+            <button onClick={props.handleJobHistory}>
+                Professional
+            </button>
             <ul className='historyList'>
                 <li className='jobEra'>
                 <h4>Pedersen Solutions</h4>
                     <p className='jobEraDescription'>
 
-                        <img src= {require('../../images/Pedersen.png')} alt='Company Icon'/> 
+                        <img className='smImg' src= {require('../../images/Pedersen.jpg')} alt='Company Icon'/> 
                         After returning home to Winter Park after graduating from Galvanize I created my own company, Pedersen Solutions to meet the demands of modern Web Development for small businesses.
                         I specialize in bringing my clients afforable solutions and enjoy teaching them how they can use Social Media and platforms like Google Tag Manager, Anayltics and AdWords themselves to
                         make their business more successful.  
@@ -25,7 +23,7 @@ const About = () => {
                     <h4>The Foundry Cinema and Bowl</h4>
                         <p className='jobEraDescription'>
 
-                            <img src= {require('../../images/Pedersen.png')} alt='Company Icon'/> 
+                            <img className='smImg' src= {require('../../images/Foundry.jpg')} alt='Company Icon'/> 
                             From 2012-2019 I worked at the Foundry Cinema and Bowl in Fraser, Co. I began bartending and serving while helping manage the front-of-house operations. Eventually helped oversee hospitality as a whole as well as personel management, day to day operations, and technology driven media and business solutions. 
                         </p>
                 </li>
@@ -33,7 +31,7 @@ const About = () => {
                     <h4>Studio 401 Salon</h4>
                         <p className='jobEraDescription'>
 
-                            <img src= {require('../../images/Pedersen.png')} alt='Company Icon'/> 
+                            <img className='smImg' src= {require('../../images/401.jpg')} alt='Company Icon'/> 
                             Studio 401 Salon is a Winter Park, CO's premier modern salon specializing in hair, waxing, and tanning services.
                             In 2016, my then girlfriend (now just business parter) and I decided that due to her success as a stylist with a large customer base that we wanted to go into business for ourselves and create the best modern salon in the Grand County area. We handled the build out, intereior decorating, infrastructre, customer database integration, scheduling services, identity design, web design, Google Analytics, Google Adwords/AdWordsExpress, SEO, brand and marketing management all our selves.
                             We used Squarespace for web design and deployment due to its ease of use and responsive mobile design due to at the time we were a bit short on time working full time, raising two boys all while and putting the whole salon together in 4 weeks.
@@ -43,7 +41,7 @@ const About = () => {
                     <h4>Mountain Chalet Propery Managment</h4>
                         <p className='jobEraDescription'>
 
-                            <img src= {require('../../images/Pedersen.png')} alt='Company Icon'/> 
+                            <img className='smImg' src= {require('../../images/mountainChalet.jpg')} alt='Company Icon'/> 
                             I began working at Mountain Chalet Property Management in the summer of 2007. 
                             Through the years I handled many task from landscaping, building upkeep and repair, winterizing properties, snow removal operations, painting, and day to day property upkeep. I left the company full time in 2011 and then returned as a consultant in 2012-13 until Dick and Louise Norman sold the company in 2014.
                         </p>
@@ -51,9 +49,9 @@ const About = () => {
             </ul>
                 </div>
           <div className='pageCard'>
-            <h3>
-                Personal History
-            </h3>
+            <button>
+                Personal 
+            </button>
                 <p>Lorem ninja ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut ninja wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit ninja lobortis nisl ut aliquip ex ea commodo consequat. Duis ninja autem vel eum iriure dolor in hendrerit in vulputate ninja velit esse molestie consequat, vel illum dolore eu feugiat nulla ninja</p>
           </div>
         </div>
